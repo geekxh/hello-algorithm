4912e6ba2e58ef35020398f017f45dc091fb355a
haolin3 <haolin3@creditease.corp>
2020-06-12 16:06:50 +0800

    refactor

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: "小浩算法",
+    plugins: [require('../../lib/autobar.js')],
+    description: '和小浩一起学算法',
+    root : "./hell-algorithm",
+    theme: "reco",
+    themeConfig: {
+        sidebar: "auto",
+        nav: [
+            { text: 'GitHub', link: 'https://github.com/geekxh/hello-algorithm', icon: 'reco-github' },
+        ],
+        type: 'blog',
+        search: true,
+        searchMaxSuggestions: 10,
+        lastUpdated: '文章修订于',
+        author: '程序员小浩',
+        authorAvatar: '/avatar.png',
+        record: 'xxxx',
+        startYear: '2019',
+        valineConfig: {
+            appId: '',
+            appKey: '',
+        }
+    },
+    markdown: {
+        lineNumbers: true,
+    }
+}
