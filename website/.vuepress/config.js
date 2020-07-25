616111986ba9e41618dd2aadd3323033026576ce
geekxh <516729107@qq.com>
2020-07-25 12:04:39 +0800

    fix bug

--- website/.vuepress/config.js
+++ website/.vuepress/config.js
@@ -1,39 +1,38 @@
 module.exports = {
-    title: "小浩算法",
-    plugins: [require('../../lib/autobar.js'), require('../../lib/Notification.js')],
-    description: '和小浩一起学算法',
-    // root : "./hell-algorithm",
-    theme: "reco",
-    themeConfig: {
-        logo: '/logo.jpeg',
-        sidebar: "auto",
-        searchPlaceholder: "搜索：KMP",
-        nav: [
-            {text: '动态规划', link: '/1.2.动态规划系列' },
-            {text: 'GitHub', link: 'https://github.com/geekxh/hello-algorithm', icon: 'reco-github'}
-        ],
-        head:[
-            ['link',{rel:'shortcut icon',href:'/favicon.ico'}]
-        ],
-        type: 'blog',
-        search: true,
-        searchMaxSuggestions: 10,
-        lastUpdated: '文章修订于',
-        author: '程序员小浩',
-        authorAvatar: '/code.png',
-        startYear: '2019',
-        valineConfig: {
-            visitor:false,
-            enable: false,
-            placeholder: "留下你的问题，或者你想要的资源...",
-            avatar: "wavatar",
-            requiredFields: ['nick'],
-            appId: 'sINkW5sfpPxoqDyBqbpvTN79-gzGzoHsz',
-            appKey: '5tkDVulRNyjuAbCUekxU6zHW',
-        },
-        noFoundPageByTencent: false
+  title: "小浩算法",
+  plugins: [require('../../lib/autobar.js'), require('../../lib/Notification.js')],
+  description: '和小浩一起学算法',
+  // root : "./hell-algorithm",
+  theme: "reco",
+  themeConfig: {
+    logo: '/logo.png',
+    sidebar: "auto",
+    searchPlaceholder: "搜索：KMP",
+    nav: [
+      {text: 'GitHub', link: 'https://github.com/geekxh/hello-algorithm', icon: 'reco-github'}
+    ],
+    head: [
+      ['link', {rel: 'shortcut icon', href: '/favicon.ico'}]
+    ],
+    type: 'blog',
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: '文章修订于',
+    author: '程序员小浩',
+    authorAvatar: '/code.png',
+    startYear: '2019',
+    valineConfig: {
+      visitor: false,
+      enable: true,
+      placeholder: "留下你的问题，或者你想要的资源...",
+      avatar: "wavatar",
+      requiredFields: ['nick'],
+      appId: 'sINkW5sfpPxoqDyBqbpvTN79-gzGzoHsz',
+      appKey: '5tkDVulRNyjuAbCUekxU6zHW',
     },
-    markdown: {
-        lineNumbers: true,
-    }
+    noFoundPageByTencent: false
+  },
+  markdown: {
+    lineNumbers: true,
+  }
 }
